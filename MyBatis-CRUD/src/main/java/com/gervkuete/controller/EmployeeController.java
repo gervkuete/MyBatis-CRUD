0eaa55c65be749c579cdcfe3e28e0e73813a7890
Gervais <lepetitgervais1@gmail.com>
2019-03-25 00:07:27 +0300

    Project initialization.

---
+++ MyBatis-CRUD/src/main/java/com/gervkuete/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.gervkuete.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+	
+	private Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+	//logger.info("Inside controller");
+	
+
+}
